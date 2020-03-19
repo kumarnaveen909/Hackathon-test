ffed9817def69c8350799f3c24dde5cb666effd9
kumarnaveen909 <59497392+kumarnaveen909@users.noreply.github.com>
2020-03-19 12:36:30 +0530

    Add files via upload

---
+++ target/cucumber-reports.html/report.js
@@ -0,0 +1,76 @@
+$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("file:src/test/java/com/ui/automation/features/Testfeature.feature");
+formatter.feature({
+  "name": "Validate the new price information after applying the promocode if not applied before",
+  "description": "",
+  "keyword": "Feature"
+});
+formatter.scenario({
+  "name": "Check the price of selected item in the cart",
+  "description": "",
+  "keyword": "Scenario"
+});
+formatter.step({
+  "name": "Launch the application URL",
+  "keyword": "Given "
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.step({
+  "name": "Mouse over for selecting All Categories link",
+  "keyword": "When "
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.step({
+  "name": "Select Offers link",
+  "keyword": "And "
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.step({
+  "name": "Select Health and Safety",
+  "keyword": "And "
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.step({
+  "name": "Clicking on a link under Health and Safety",
+  "keyword": "And "
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.step({
+  "name": "Select an item",
+  "keyword": "And "
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.step({
+  "name": "Capture the price of the selected item",
+  "keyword": "And "
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.step({
+  "name": "Goto the cart and view he cart",
+  "keyword": "And "
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+});
